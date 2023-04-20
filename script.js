e1c0a287a3beacdbc77d611975584b0a829ede22
Atanu Paul <atanupaul2411@gmail.com>
2023-04-20 08:57:49 +0530

    bug fixed

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ function event_time() {
         let events = [
             {
                 date: time.getDate(),
-                hour: time.getHours() + 2,
+                // hour: time.getHours() + 2,
+                hour: 0,
                 min: time.getMinutes() + 5,
                 sec: time.getSeconds() + 35,
                 am_pm: 'AM'
             },
             {
                 date: time.getDate(),
-                hour: time.getHours() + 3,
+                hour: time.getHours() + 4,
                 min: time.getMinutes() + 7,
                 sec: time.getSeconds() + 35,
                 am_pm: 'AM'
@@ -45,11 +46,14 @@ function event_time() {
             events[i].min -= 60
             events[i].hour += 1
         }
-        if ((events[i].hour + 1) > 11 && (events[i].hour + 1) <= 23) {
+        if ((events[i].hour ) > 11 && (events[i].hour ) <= 23) {
             events[i].am_pm = 'PM'
         }
         if(events[i].hour >= 24) {
             events[i].hour -= 24
+        }
+        if(events[i].hour == 0 && events[i].min >= 0 && events[i].min <= 59){
+            events[i].hour += 12
         }
         if (events[i].hour > 12) {
             events[i].hour -= 12
@@ -64,28 +68,24 @@ function event_time() {
 }
 
 event_time();
-setInterval(event_time, 3600000);
 
-function count(e_hour, e_min, e_sec, c_no) {
+function count(e_hour, e_min, e_sec, c_no, am_pm) {
     let time = new Date();
 
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-
-    if (sec > 59) {
-        sec -= 60
-        min += 1
-    }
-    if (min > 59) {
-        min -= 60
-        hour += 1
-    }
     
+    // let hour_left = (am_pm == 'PM') ? parseInt(e_hour - hour) + 12 : (e_hour - hour);
+    let hour_left = (e_hour - hour);
 
-    let hour_left = parseInt(e_hour - hour) + 12
-    let min_left = e_min - min
-    let sec_left = e_sec - sec
+    if (am_pm == "PM") {
+        hour_left = parseInt(e_hour - hour) + 12;
+    }
+
+    
+    let min_left = e_min - min;
+    let sec_left = e_sec - sec;
 
     if (sec_left < 0) {
         sec_left += 60
@@ -118,8 +118,9 @@ button.forEach((btn, i) => {
             countdown[i].innerHTML = `Event Time: ${cards[0].innerHTML}`;
             let e_hour = cards[0].innerHTML.slice(0, 2);
             let e_min = cards[0].innerHTML.slice(3, 5);
+            let am_pm = cards[0].innerHTML.slice(-2);
             let e_sec = 35;
-            setInterval(function () { count(e_hour, e_min, e_sec, 0), 1000 })
+            setInterval(function () { count(e_hour, e_min, e_sec, 0, am_pm);}, 1000 );
             event1.style.display = 'block';
             event2.style.display = 'none';
             event3.style.display = 'none';
@@ -129,8 +130,9 @@ button.forEach((btn, i) => {
             countdown[i].innerHTML = `Event Time: ${cards[1].innerHTML}`;
             let e_hour = cards[1].innerHTML.slice(0, 2);
             let e_min = cards[1].innerHTML.slice(3, 5);
+            let am_pm = cards[1].innerHTML.slice(-2);
             let e_sec = 25;
-            setInterval(function () { count(e_hour, e_min, e_sec, 1), 1000 });
+            setInterval(function () { count(e_hour, e_min, e_sec, 1, am_pm);}, 1000 );
             event1.style.display = 'none';
             event2.style.display = 'block';
             event3.style.display = 'none';
@@ -140,8 +142,9 @@ button.forEach((btn, i) => {
             countdown[i].innerHTML = `Event Time: ${cards[2].innerHTML}`;
             let e_hour = cards[2].innerHTML.slice(0, 2);
             let e_min = cards[2].innerHTML.slice(3, 5);
+            let am_pm = cards[2].innerHTML.slice(-2);
             let e_sec = 45;
-            setInterval(function () { count(e_hour, e_min, e_sec, 2), 1000 });
+            setInterval(function () { count(e_hour, e_min, e_sec, 2, am_pm);}, 1000 );
             event1.style.display = 'none';
             event2.style.display = 'none';
             event3.style.display = 'block';
@@ -151,8 +154,9 @@ button.forEach((btn, i) => {
             countdown[i].innerHTML = `Event Time: ${cards[3].innerHTML}`;
             let e_hour = cards[3].innerHTML.slice(0, 2);
             let e_min = cards[3].innerHTML.slice(3, 5);
+            let am_pm = cards[3].innerHTML.slice(-2);
             let e_sec = 55;
-            setInterval(function () { count(e_hour, e_min, e_sec, 3), 1000 });
+            setInterval(function () { count(e_hour, e_min, e_sec, 3, am_pm);}, 1000 );
             event1.style.display = 'none';
             event2.style.display = 'none';
             event3.style.display = 'none';
